c679178be4e9bf7f3f14239436d2be7d0babbdbe
Ian Norris <icstatic@gmail.com>
2015-12-18 01:30:05 +0000

    All dependencies now compiling and restclient unit tests now run build and run.

--- vendor/build/vs2015/build-dependencies.bat
+++ vendor/build/vs2015/build-dependencies.bat
@@ -3,11 +3,17 @@
 for %%X in (msbuild.exe) do (set FOUND=%%~$PATH:X)
 if not defined FOUND call "%VS140COMNTOOLS%\VsDevCmd.bat"
 
+cd /D %~dp0/../../curl/winbuild
+
+nmake /f Makefile.vc mode=static VC=14 ENABLE_WINSSL=yes GEN_PDB=yes MACHINE=x86
+nmake /f Makefile.vc mode=static VC=14 ENABLE_WINSSL=yes GEN_PDB=yes MACHINE=x86 DEBUG=yes
+REM nmake /f Makefile.vc mode=static VC=14 ENABLE_WINSSL=yes GEN_PDB=yes MACHINE=x64
+
 cd /D %~dp0
 
 msbuild /t:Build /p:Configuration="Debug" /p:Platform="Win32" dependencies.vcxproj
-msbuild /t:Build /p:Configuration="Debug" /p:Platform="x64" dependencies.vcxproj
+REM msbuild /t:Build /p:Configuration="Debug" /p:Platform="x64" dependencies.vcxproj
 msbuild /t:Build /p:Configuration="Release" /p:Platform="Win32" dependencies.vcxproj
-msbuild /t:Build /p:Configuration="Release" /p:Platform="x64" dependencies.vcxproj
+REM msbuild /t:Build /p:Configuration="Release" /p:Platform="x64" dependencies.vcxproj
 
 pause
